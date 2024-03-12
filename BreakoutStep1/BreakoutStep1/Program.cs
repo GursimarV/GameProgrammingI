26a0030d9d83a896a35138b8049dc2c60ce96ca7
Gursimar Virdi <385491@colum.edu>
2024-03-12 15:30:24 -0500

    Midterm and Changes

---
+++ BreakoutStep1/BreakoutStep1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BreakoutStep1
+{
+    //Code provided by Jeff Meyers
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
